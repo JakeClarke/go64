01025ad224850728fd5dd22371f5e9e4d7df9365
Jake Clarke <clarke.jake@gmail.com>
2013-05-30 14:48:23 +0100

    Added documentation.

--- cpu/mem.go
+++ cpu/mem.go
@@ -2,16 +2,19 @@ package cpu
 
 type Memory [0x8000]byte
 
+// Store word stores a word to the memory.
 func (m *Memory) StoreWord(addr DWORD, value WORD) {
 	m.StoreHalfWord(addr+2, HWORD(value>>16))
 	m.StoreHalfWord(addr, HWORD(value))
 }
 
+// Store halfword stores a halfword to the memory.
 func (m *Memory) StoreHalfWord(addr DWORD, value HWORD) {
 	m[addr+1] = byte(value >> 8)
 	m[addr] = byte(value)
 }
 
+// Load word loads a word from the memory.
 func (m *Memory) LoadWord(addr DWORD) WORD {
 	var res WORD
 
@@ -21,6 +24,7 @@ func (m *Memory) LoadWord(addr DWORD) WORD {
 	return res
 }
 
+// Load halfword loads a halfword from the memory.
 func (m *Memory) LoadHalfWord(addr DWORD) HWORD {
 	var res HWORD
 
@@ -30,16 +34,19 @@ func (m *Memory) LoadHalfWord(addr DWORD) HWORD {
 	return res
 }
 
+// Load bytes unsigned loads an unsigned byte.
 func (m *Memory) LoadByteU(addr DWORD) uint8 {
 	return uint8(m[addr])
 }
 
+// Init resets all memory locations to 0.
 func (m *Memory) Init() {
 	for index, _ := range m {
 		m[index] = 0x00
 	}
 }
 
+// SetBytes sets large numbers of bytes.
 func (m *Memory) SetBytes(addr DWORD, bytes []uint8) {
 	for index, v := range bytes {
 		m[addr+DWORD(index)] = v
